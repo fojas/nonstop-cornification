9e0e2ff9d3d85d08158d1bfe5ca77b175a4aee81
Fojas <developer@fojasaur.us>
2012-05-13 16:47:04 -0500

    use port from environment if provided

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.configure(function(){
 
 app.get('/', function(req, res){res.render('index', { title: 'Cornify' })});
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
