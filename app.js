af0554389e96b4383e6dcb3a4716177bebcc7e23
Fojas <developer@fojasaur.us>
2012-05-13 16:42:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+  , cornify = require('cornify');
+
+var app = module.exports = express.createServer();
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(cornify({interval:2000}));
+  app.use(app.router);
+});
+
+app.get('/', function(req, res){res.render('index', { title: 'Cornify' })});
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
